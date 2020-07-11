39e8d0b419445996e473ea04cbdcbe9b81233e37
Admin <admin@joseph-test-1.myfiosgateway.com>
2020-07-11 03:14:58 -0400

    trying to get prod right

--- src/server/index.js
+++ src/server/index.js
@@ -2,11 +2,14 @@ const express = require('express');
 const os = require('os');
 const bodyParser = require('body-parser')
 const db = require('./dao')
+const path = require('path');
+
 //const svc = require('./svc')
 
 const app = express();
 
-app.use(express.static('dist'));
+// app.use(express.static('dist'));
+app.use(express.static(path.join(__dirname, 'build')));
 app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
